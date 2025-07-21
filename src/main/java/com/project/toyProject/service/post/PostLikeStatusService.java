11462a00dab31d47255e14a0c671135fb70722a3
ChoiSungWon <spancho0810@naver.com>
2025-07-21 09:25:06 +0900

    게시글 기본 2단계 완성

--- src/main/java/com/project/toyProject/service/post/PostLikeStatusService.java
+++ src/main/java/com/project/toyProject/service/post/PostLikeStatusService.java
@@ -4,7 +4,7 @@ import com.project.toyProject.domain.vo.PostLikeStatusVO;
 
 public interface PostLikeStatusService {
 //    좋아요 상태변화
-    public void toggleLikeStatus(Long postId,Long memberId);
+    void toggleLikeStatus(Long postId,Long memberId);
 //    좋아요 상태가져오기
-    public PostLikeStatusVO getPostLikeStatus(Long postId, Long memberId);
+     PostLikeStatusVO getPostLikeStatus(Long postId, Long memberId);
 }

fab85b9c023489b280f72ed18dcc3520345e3830
ChoiSungWon <spancho0810@naver.com>
2025-08-19 08:59:59 +0900

    기본 게시글 완성

--- src/main/java/com/project/toyProject/config/WebConfig.java
+++ src/main/java/com/project/toyProject/config/WebConfig.java
@@ -1,9 +1,7 @@
 package com.project.toyProject.config;
 
 import com.project.toyProject.interceptor.LoginCheckInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
